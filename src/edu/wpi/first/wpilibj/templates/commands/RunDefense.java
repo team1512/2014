bda7e94cc4212d5f2af1cf044a4d47aa9196cdfc
robots <robots@C04482.spsnet.sps.edu>
2014-02-13 12:07:52 -0500

    Jackson Dalman added the Defense class - runs a spike, operated by a and y buttons on joystick 2

---
+++ src/edu/wpi/first/wpilibj/templates/commands/RunDefense.java
@@ -0,0 +1,72 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.wpi.first.wpilibj.templates.commands;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.templates.OI;
+import edu.wpi.first.wpilibj.templates.Team1512Joystick;
+
+/**
+ *
+ * @author robot
+ */
+public class RunDefense extends CommandBase {
+    /* The defense class is controlled by xbox #2.
+     * It uses a relay to run a motor forward and backward
+     */
+    
+    public RunDefense() {
+        //reserve the defensive system
+        requires(defense);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+        //the climber is initially off
+        SmartDashboard.putString("Defense: ", "OFF");
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+        //y toggles the climber on/off
+        if (oi.stick.isButtonPressed(Team1512Joystick.XBOX_BUTTON_Y))
+        //if (oi.stick.getLTRG()) //&& blanket.getLimTop() == false
+        {
+              defense.setForward();
+                //write the state of the blanket to the Smart Dashboard
+                SmartDashboard.putString("Defense: ", "CLOCKWISE");
+                //System.out.println("Climber off");
+        } else if (oi.stick.isButtonPressed(Team1512Joystick.XBOX_BUTTON_A)) 
+        //&& blanket.getLimBot() == false
+        {
+              defense.setBackward();
+                //write the state of the blanket to the Smart Dashboard
+                SmartDashboard.putString("Defense: ", "CCLOCKWISE");
+                //System.out.println("Climber on");                                   
+        }
+        else
+        {
+            defense.turnOff();
+            SmartDashboard.putString("Defense: ", "OFF");
+        }
+        
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+        // blanket.turnOff();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+        //blanket.turnOff();
+    }
+}
